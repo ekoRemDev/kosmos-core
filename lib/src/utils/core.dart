d9a102b72b876ac8a11c835951d654a1bf1861dd
Maxence <maxence@kosmos-digital.com>
2022-06-28 14:56:40 +0200

    add package

---
+++ lib/src/utils/core.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:core_kosmos/src/theme/app_theme.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+T execInCaseOfPlatfom<T>(Function fnWeb, Function fnMobile) {
+  if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
+    return fnMobile();
+  } else {
+    return fnWeb();
+  }
+}
+
+T? getValueForPlatform<T>(T? val1, T? val2) => execInCaseOfPlatfom(() => val1, () => val2);
+
+void printInDebug(dynamic obj) {
+  if (kDebugMode) {
+    print(obj);
+  }
+}
+
+void execAfterBuild(Function fn) => WidgetsBinding.instance?.addPostFrameCallback((_) => fn());
+
+T? loadThemeData<T>(T? theme, String themeName, T Function() constructor) {
+  return theme ?? GetIt.instance<AppTheme>().fetchTheme<T>(themeName) ?? constructor() ?? theme;
+}
+
+String? enumToString<T>(T actual) {
+  return actual.toString().split(".").last;
+}
