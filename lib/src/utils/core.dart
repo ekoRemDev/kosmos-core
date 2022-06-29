04f24be47e61f961eca6505463a82aa45342a370
Maxence VASSEUR <maxence@kosmos-digital.com>
2022-06-29 09:22:00 +0000

    V1.0.0 - Bug Fix

--- lib/src/utils/core.dart
+++ lib/src/utils/core.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_null_in_if_null_operators
+
 import 'dart:io';
 
 import 'package:core_kosmos/src/theme/app_theme.dart';
@@ -24,7 +26,7 @@ void printInDebug(dynamic obj) {
 void execAfterBuild(Function fn) => WidgetsBinding.instance?.addPostFrameCallback((_) => fn());
 
 T? loadThemeData<T>(T? theme, String themeName, T Function() constructor) {
-  return theme ?? GetIt.instance<AppTheme>().fetchTheme<T>(themeName) ?? constructor() ?? theme;
+  return theme ?? (GetIt.instance.isRegistered<AppTheme>() ? GetIt.instance<AppTheme>().fetchTheme<T>(themeName) : null) ?? constructor() ?? theme;
 }
 
 String? enumToString<T>(T actual) {
